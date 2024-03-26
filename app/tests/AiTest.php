bd7780d77424865194185063e48060fdbfd8d6ad
KryLoud <f.a.hulshof@st.hanze.nl>
2024-03-26 23:04:35 +0100

    update AiTest.php

--- app/tests/AiTest.php
+++ app/tests/AiTest.php
@@ -18,20 +18,21 @@ class AiTest extends TestCase
 
 
 
-//    public function testAiFirstMove(){
-//        $board = [];
-//        $currentPlayerIndex = 0;
-//        $game_id = 9999999;  // hardcoded testId
-//        $hand = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
-//        //Because game->restart starts with a aiMove() it is not possible to get the move from restart only on the board
-//        $aiMock = $this->getMockBuilder(\App\Ai::class)->disableOriginalConstructor()
-//            ->getMock();
-//
-//        $aiMock->move($currentPlayerIndex, $board, $hand);
-//
-//
-//        $aiMove = $this->game->aiMove();
-//        var_dump($aiMove);
-//    }
+    public function testAiFirstMove(){
+        $board = [];
+        $currentPlayerIndex = 0;
+        $game_id = 9999999;  // hardcoded testId
+        $hand = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
+        //Because game->restart starts with a aiMove() it is not possible to get the move from restart only on the board
+        $aiMock = $this->getMockBuilder(\App\Ai::class)->disableOriginalConstructor()
+            ->getMock();
+
+        $aiMock->move($currentPlayerIndex, $board, $hand);
+
+        $predictedMove = ["play", "Q", "0,0"];
+        $aiMove = $this->game->aiMove();
+
+        $this->assertEquals($predictedMove, $aiMove);
+    }
 
 }
