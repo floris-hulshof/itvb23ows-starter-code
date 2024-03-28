7164c8dcc863959fe168672f20bc4620a74683bb
KryLoud <f.a.hulshof@st.hanze.nl>
2024-03-28 22:40:40 +0100

    Ai test fix

--- app/tests/AiTest.php
+++ app/tests/AiTest.php
@@ -9,39 +9,31 @@ use PHPUnit\Framework\TestCase;
 class AiTest extends TestCase
 {
     private $game;
-    public function __construct(string $name)
+    protected function setUp(): void
     {
-        parent::__construct($name);
-        $db= new DatabaseMock();
-        $this->game = new Game($db, true);
+        $db = new DatabaseMock();
+        $this->game = new Game($db, false);
     }
 
-
     public function testAiIsFirstMove(){
-//        $aiMock = $this->getMockBuilder(\App\Ai::class)->disableOriginalConstructor()
-//            ->getMock();
+
         //when game restarts ai will immediately play and switch to player 2
-        $this->game->testRestart();
+        $aiMock = $this->getMockBuilder(\App\Ai::class)->disableOriginalConstructor()
+            ->getMock();
+        $this->game->aiMove();
         $currentPlayer = $this->game->getCurrentPlayerIndex();
-        var_dump($currentPlayer);
         $expectedPlayer = 1;
         $this->assertEquals($expectedPlayer, $currentPlayer);
     }
 
     public function testAiFirstMove(){
-        $board = [];
-        $currentPlayerIndex = 0;
-        $game_id = 9999999;  // hardcoded testId
-        $hand = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
-        //Because game->restart starts with a aiMove() it is not possible to get the move from restart only on the board
-//        $this->game->testRestart();
+
         $aiMock = $this->getMockBuilder(\App\Ai::class)->disableOriginalConstructor()
             ->getMock();
 
-        $aiMock->move($currentPlayerIndex, $board, $hand);
+        $aiMove =$aiMock->move($this->game->getCurrentPlayerIndex(), [$this->game->getPlayerHand(0), $this->game->getPlayerHand(1)], $this->game->getBoard());
 
         $predictedMove = ["play", "Q", "0,0"];
-        $aiMove = $this->game->aiMove();
 
         $this->assertEquals($predictedMove, $aiMove);
     }
